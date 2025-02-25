8d43a96614c7fd4f096da7232a299fec4b301402
Goshanije <158315472+Goshanije@users.noreply.github.com>
2025-02-25 20:40:50 +0500

    Delete директория сайта directory

--- директория сайта/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.details-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        const details = button.nextElementSibling;
-        details.style.display = details.style.display === 'block' ? 'none' : 'block';
-    });
-});
