3efe28dafbe5bfffa69953221242a6143c36f87c
Goshanije <158315472+Goshanije@users.noreply.github.com>
2026-07-03 19:51:46 +0500

    update from homework

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ document.querySelectorAll('.details-btn').forEach(button => {
         details.style.display = details.style.display === 'block' ? 'none' : 'block';
     });
 });
+
+const toggleBtn = document.getElementById('theme-toggle');
+
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        toggleBtn.textContent = 'Light theme';
+    } else {
+        toggleBtn.textContent = 'Dark theme';
+    }
+
+    toggleBtn.addEventListener('click', () => {
+        let theme = document.documentElement.getAttribute('data-theme');
+        
+        if (theme === 'dark') {
+            document.documentElement.removeAttribute('data-theme');
+            localStorage.setItem('theme', 'light');
+            toggleBtn.textContent = 'Dark theme';
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+            toggleBtn.textContent = 'Light theme';
+        }
+    });
